21f2ac85f14fd8349de46903cf63bc1be5ecb988
CatherineCLCLCL <catherineclclcl@gmail.com>
2022-03-14 08:31:14 +0800

    Minor updates

--- plot_regression_randombag_sample_average.py
+++ plot_regression_randombag_sample_average.py
@@ -723,14 +723,18 @@ def plot_equity_explained_variance(demo_feat, show_yticks=True):
         plt.yticks(np.arange(9)*step_1-0.5*step_2,empty_list, fontsize=20)  
     #plt.xlim(0,1)
     plt.xticks(fontsize=14) 
+    plt.xlabel(f'Explained variance\n',fontsize=18) #20220313
+    '''
     if(demo_feat=='minority'):
         plt.xlabel(f'Explained variance\nof equity-by-race-ethnicity',fontsize=18)
     else:
         plt.xlabel(f'Explained variance\nof equity-by-{demo_feat}',fontsize=18)
+    '''
     # Save figure
     savepath = os.path.join(saveroot, 'figures', f'fig3c_{demo_feat}.pdf')
     plt.savefig(savepath,bbox_inches = 'tight')
     print(f'Fig3c_{demo_feat}, saved at {savepath}')
+    
 
 # Fig3c, age
 plot_equity_explained_variance(demo_feat='age', show_yticks=True)
