244b127b4ea50a2115edef037527587c23e5c43d
CatherineCLCLCL <catherineclclcl@gmail.com>
2022-03-15 08:23:42 +0800

    Create epidemic_simulation_test.py
    for estimating reproduction number

---
+++ epidemic_simulation_test.py
@@ -0,0 +1,243 @@
+# python epidemic_simulation_test.py SanFrancisco
+
+import setproctitle
+setproctitle.setproctitle("gnn-simu-vac@chenlin")
+
+import sys
+
+import os
+import datetime
+import pandas as pd
+import numpy as np
+import pickle
+
+import constants
+import functions
+#import disease_model #disease_model_only_modify_attack_rates
+import disease_model_test_r0 as disease_model
+
+import pdb
+
+###############################################################################
+# Constants
+
+epic_data_root = '/data/chenlin/COVID-19/Data'
+gt_result_root = os.getcwd()
+
+MIN_DATETIME = datetime.datetime(2020, 3, 1, 0)
+MAX_DATETIME = datetime.datetime(2020, 5, 2, 23)
+NUM_DAYS = 63
+
+# Vaccination ratio
+VACCINATION_RATIO = 0#0.02
+print('VACCINATION_RATIO: ', VACCINATION_RATIO)
+
+# Vaccination protection rate
+PROTECTION_RATE = 1
+# Policy execution ratio
+EXECUTION_RATIO = 1
+# Vaccination time
+VACCINATION_TIME = 0 #31
+print('VACCINATION_TIME: ', VACCINATION_TIME)
+
+###############################################################################
+# Main variable settings
+
+MSA_NAME = sys.argv[1]; print('MSA_NAME: ',MSA_NAME) #MSA_NAME = 'SanFrancisco'
+MSA_NAME_FULL = constants.MSA_NAME_FULL_DICT[MSA_NAME] #MSA_NAME_FULL = 'San_Francisco_Oakland_Hayward_CA'
+
+# Quick Test: prototyping
+quick_test = sys.argv[2]; print('Quick testing?', quick_test)
+if(quick_test == 'True'):
+    NUM_SEEDS = 2
+else:
+    NUM_SEEDS = 30 #60 #30
+print('NUM_SEEDS: ', NUM_SEEDS)
+STARTING_SEED = range(NUM_SEEDS)
+
+###############################################################################
+# Functions
+
+def run_simulation(starting_seed, num_seeds, vaccination_vector, vaccine_acceptance, protection_rate=1):
+    m = disease_model.Model(starting_seed=starting_seed,
+                            num_seeds=num_seeds,
+                            debug=False,clip_poisson_approximation=True,ipf_final_match='poi',ipf_num_iter=100)
+
+    m.init_exogenous_variables(poi_areas=poi_areas,
+                               poi_dwell_time_correction_factors=poi_dwell_time_correction_factors,
+                               cbg_sizes=cbg_sizes,
+                               poi_cbg_visits_list=poi_cbg_visits_list,
+                               all_hours=all_hours,
+                               p_sick_at_t0=constants.parameters_dict[MSA_NAME][0],
+                               #vaccination_time=24*31, # when to apply vaccination (which hour)
+                               vaccination_time=24*VACCINATION_TIME, # when to apply vaccination (which hour)
+                               vaccination_vector = vaccination_vector,
+                               vaccine_acceptance=vaccine_acceptance,
+                               protection_rate = protection_rate,
+                               home_beta=constants.parameters_dict[MSA_NAME][1],
+                               cbg_attack_rates_original = cbg_attack_rates_scaled,
+                               cbg_death_rates_original = cbg_death_rates_scaled,
+                               poi_psi=constants.parameters_dict[MSA_NAME][2],
+                               just_compute_r0=True, #20220314 #False,
+                               latency_period=96,  # 4 days
+                               infectious_period=84,  # 3.5 days
+                               confirmation_rate=.1,
+                               confirmation_lag=168,  # 7 days
+                               death_lag=432
+                               )
+
+    m.init_endogenous_variables()
+
+    #T1,L_1,I_1,R_1,C2,D2,total_affected, history_C2, history_D2, total_affected_each_cbg = m.simulate_disease_spread(no_print=True)    
+    history_C2, history_D2 = m.simulate_disease_spread(no_print=True)    
+    
+    #return total_affected, history_C2, history_D2, total_affected_each_cbg
+    return history_C2, history_D2
+
+
+###############################################################################
+# Load Data
+
+# Load POI-CBG visiting matrices
+f = open(os.path.join(epic_data_root, MSA_NAME, '%s_2020-03-01_to_2020-05-02.pkl'%MSA_NAME_FULL), 'rb') 
+poi_cbg_visits_list = pickle.load(f)
+f.close()
+
+#######################################################
+just_compute_r0 = True # 20220314
+
+if just_compute_r0:
+    print('Prelockdown mobility.')
+    print('Running model to compute r0 -> looping first week visit counts')
+    # simulate out 15 weeks just so we are sure all cases are gone.
+    MAX_DATETIME = MIN_DATETIME + datetime.timedelta(hours=(168*15)-1)
+    all_hours = functions.list_hours_in_range(MIN_DATETIME, MAX_DATETIME)
+    print("Extending time period; simulation now ends at %s (%d hours)" % (max(all_hours), len(all_hours)))
+    if poi_cbg_visits_list is not None:
+        assert len(poi_cbg_visits_list) >= 168  # ensure that we have at least a week to model
+        new_visits_list = []
+        for i in range(168 * 15):
+            first_week_idx = i % 168  # map to corresponding hour in first week
+            new_visits_list.append(poi_cbg_visits_list[first_week_idx].copy())
+        poi_cbg_visits_list = new_visits_list
+        assert len(poi_cbg_visits_list) == len(all_hours)
+    else: 
+        pdb.set_trace()
+#######################################################
+
+
+# Load precomputed parameters to adjust(clip) POI dwell times
+d = pd.read_csv(os.path.join(epic_data_root,MSA_NAME, 'parameters_%s.csv' % MSA_NAME)) 
+
+# No clipping
+new_d = d
+
+all_hours = functions.list_hours_in_range(MIN_DATETIME, MAX_DATETIME)
+poi_areas = new_d['feet'].values#面积
+poi_dwell_times = new_d['median'].values#平均逗留时间
+poi_dwell_time_correction_factors = (poi_dwell_times / (poi_dwell_times+60)) ** 2
+del new_d
+del d
+
+# Load ACS Data for MSA-county matching
+acs_data = pd.read_csv(os.path.join(epic_data_root,'list1.csv'),header=2)
+acs_msas = [msa for msa in acs_data['CBSA Title'].unique() if type(msa) == str]
+msa_match = functions.match_msa_name_to_msas_in_acs_data(MSA_NAME_FULL, acs_msas)
+msa_data = acs_data[acs_data['CBSA Title'] == msa_match].copy()
+msa_data['FIPS Code'] = msa_data.apply(lambda x : functions.get_fips_codes_from_state_and_county_fp((x['FIPS State Code']),x['FIPS County Code']), axis=1)
+good_list = list(msa_data['FIPS Code'].values) #;print('CBG included: ', good_list)
+del acs_data
+
+# Load CBG ids for the MSA
+cbg_ids_msa = pd.read_csv(os.path.join(epic_data_root,MSA_NAME,'%s_cbg_ids.csv'%MSA_NAME_FULL)) 
+cbg_ids_msa.rename(columns={"cbg_id":"census_block_group"}, inplace=True)
+M = len(cbg_ids_msa)
+
+# Mapping from cbg_ids to columns in hourly visiting matrices
+cbgs_to_idxs = dict(zip(cbg_ids_msa['census_block_group'].values, range(M)))
+x = {}
+for i in cbgs_to_idxs:
+    x[str(i)] = cbgs_to_idxs[i]
+print('Number of CBGs in this metro area:', M)
+
+# Load SafeGraph data to obtain CBG sizes (i.e., populations)
+filepath = os.path.join(epic_data_root,"safegraph_open_census_data/data/cbg_b01.csv")
+cbg_agesex = pd.read_csv(filepath)
+# Extract CBGs belonging to the MSA - https://covid-mobility.stanford.edu//datasets/
+cbg_age_msa = pd.merge(cbg_ids_msa, cbg_agesex, on='census_block_group', how='left')
+del cbg_agesex
+# Add up males and females of the same age, according to the detailed age list (DETAILED_AGE_LIST)
+# which is defined in constants.py
+for i in range(3,25+1): # 'B01001e3'~'B01001e25'
+    male_column = 'B01001e'+str(i)
+    female_column = 'B01001e'+str(i+24)
+    cbg_age_msa[constants.DETAILED_AGE_LIST[i-3]] = cbg_age_msa.apply(lambda x : x[male_column]+x[female_column],axis=1)
+# Rename
+cbg_age_msa.rename(columns={'B01001e1':'Sum'},inplace=True)
+# Extract columns of interest
+columns_of_interest = ['census_block_group','Sum'] + constants.DETAILED_AGE_LIST
+cbg_age_msa = cbg_age_msa[columns_of_interest].copy()
+# Deal with NaN values
+cbg_age_msa.fillna(0,inplace=True)
+# Deal with CBGs with 0 populations
+cbg_age_msa['Sum'] = cbg_age_msa['Sum'].apply(lambda x : x if x!=0 else 1)
+
+# Obtain cbg sizes (populations)
+cbg_sizes = cbg_age_msa['Sum'].values
+cbg_sizes = np.array(cbg_sizes,dtype='int32')
+print('Total population: ',np.sum(cbg_sizes))
+
+# Select counties belonging to the MSA
+y = []
+for i in x:
+    if((len(i)==12) & (int(i[0:5])in good_list)):
+        y.append(x[i])
+    if((len(i)==11) & (int(i[0:4])in good_list)):
+        y.append(x[i])
+        
+idxs_msa_all = list(x.values())
+idxs_msa_nyt = y
+print('Number of CBGs in this metro area:', len(idxs_msa_all))
+print('Number of CBGs in to compare with NYT data:', len(idxs_msa_nyt))
+
+nyt_included = np.zeros(len(idxs_msa_all))
+for i in range(len(nyt_included)):
+    if(i in idxs_msa_nyt):
+        nyt_included[i] = 1
+cbg_age_msa['NYT_Included'] = nyt_included.copy()
+
+##############################################################################
+# Load and scale age-aware CBG-specific attack/death rates (original)
+
+cbg_death_rates_original = np.loadtxt(os.path.join(epic_data_root, MSA_NAME, 'cbg_death_rates_original_'+MSA_NAME))
+cbg_attack_rates_original = np.ones(cbg_death_rates_original.shape)
+print('Age-aware CBG-specific death rates loaded. Attack rates are irrelevant to age.')
+
+# The scaling factors are set according to a grid search
+# Fix attack_scale
+attack_scale = 1
+cbg_attack_rates_scaled = cbg_attack_rates_original * attack_scale
+cbg_death_rates_scaled = cbg_death_rates_original * constants.death_scale_dict[MSA_NAME]
+print('Age-aware CBG-specific death rates scaled.')
+
+###############################################################################
+# No_vaccination
+
+vaccination_vector_no_vaccination = np.zeros(len(cbg_sizes))
+vaccine_acceptance = np.ones(len(cbg_sizes))
+# Run simulations
+#history_C2_no_vaccination, history_D2_no_vaccination = run_simulation(starting_seed=STARTING_SEED, num_seeds=NUM_SEEDS, 
+history_C2, history_D2 = run_simulation(starting_seed=STARTING_SEED, num_seeds=NUM_SEEDS, #20220304
+                                        vaccination_vector=vaccination_vector_no_vaccination, 
+                                        vaccine_acceptance=vaccine_acceptance,
+                                        protection_rate = PROTECTION_RATE)
+
+# Average history records across random seeds
+#cases_cbg_no_vaccination, deaths_cbg_no_vaccination, _, _ = functions.average_across_random_seeds(history_C2_no_vaccination, history_D2_no_vaccination,
+#                                                                     M, idxs_msa_nyt, 
+#                                                                     print_results=False,draw_results=False)
+
+#savepath = os.path.join(gt_result_root, 'cases_cbg_no_vaccination_%s_%sseeds.npy' % (MSA_NAME, NUM_SEEDS))
+#np.save(savepath, cases_cbg_no_vaccination)
+#savepath = os.path.join(gt_result_root, 'deaths_cbg_no_vaccination_%s_%sseeds.npy' % (MSA_NAME, NUM_SEEDS))
+#np.save(savepath, deaths_cbg_no_vaccination)
